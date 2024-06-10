f32befd993486b6920a79119b25aefc66329fb4d
hostnes <hostnes@hostness-MacBook-Pro.local>
2024-06-10 16:15:20 +0300

    fix

--- server/app/admin.py
+++ server/app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from app.models import Teacher
+from app.models import Teacher, Post
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ("name",)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title",)
